ee5127d37124c0f3c2df50d2a3c5319ce5a229d7
jayseeg <jake_gibbons@the1stmovement.com>
2012-04-04 01:57:15 -0600

    re-enabled large return set from flickr, sorry Randy, didn't see your random id selection

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -147,7 +147,7 @@ class PagesController extends AppController {
 
 		$apiKey = '9807afb683f16f992c1a2d2ee8bf2b49';
 
-		$search = 'http://www.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&tag_mode=any&api_key=' . $apiKey . '&tags=' . str_replace('%2C',',',urlencode($query)) . '&license=1,2,5,7&per_page=1&format=php_serial';
+		$search = 'http://www.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&tag_mode=any&api_key=' . $apiKey . '&tags=' . str_replace('%2C',',',urlencode($query)) . '&license=1,2,5,7&per_page=200&format=php_serial';
 
 
 		$ch = curl_init($search);
