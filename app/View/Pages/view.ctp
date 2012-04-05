be8b089478024611206ebc090f9cc2d404f8081f
jayseeg <jake_gibbons@the1stmovement.com>
2012-04-05 17:35:42 -0600

    added Artificial Social Defer to script, stripped out in markup scripts

--- app/View/Pages/view.ctp
+++ app/View/Pages/view.ctp
@@ -31,23 +31,5 @@
 	<span itemprop="name" class="visuallyhidden">Haikwitter</span>
 	<span itemprop="description" class="visuallyhidden"><?php echo $entry['Haiku']['line_1'] . " " . $entry['Haiku']['line_2'] . " " . $entry['Haiku']['line_3'];?></span>
 
-	<script>
-		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-	</script>
-	<script type="text/javascript">
-		(function() {
-			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-			po.src = 'https://apis.google.com/js/plusone.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-		})();
-	</script>
+	<script id="socialDefers"></script>
 	<div id="fb-root"></div>
-	<script>
-		(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-	</script>
--- app/webroot/js/script.js
+++ app/webroot/js/script.js
@@ -1,8 +1,8 @@
 /*********************************************************************
 *	On DOM Load - Put all js code and functions inside this function
 *	- This is also called a "self-executing anonymous function"
-* 	  it helps avoid polluting the global namespace with your 
-* 	  code — no variables declared inside of the function are 
+* 	  it helps avoid polluting the global namespace with your
+* 	  code — no variables declared inside of the function are
 * 	  visible outside of it
 **********************************************************************/
 (function($, window, document, undefined){
@@ -13,18 +13,18 @@
 	*	Put functions that need invoked after the Page is done loading
 	**********************************************************************/
 	$(window).load(function(){
-		
+
 	});
-	
+
 	/*********************************************************************
 	*	On Window Resize:
 	*	Put functions that need invoked if the browser window is resized
 	**********************************************************************/
 	$(window).resize(function(){
-		
+
 	});
-	
-	
+
+
 	/*******************************************************************
 	*	Sample Function Comment
 	*		- Just showing function structure
@@ -32,9 +32,9 @@
 	*		  what the parameters are for. NOTE: Params and Param
 	*		  explanations are examples only
 	*	Params:
-	*		@param1 - string - should be the 
+	*		@param1 - string - should be the
 	*		@param2 - boolean - heading soughtstreet
-	*	
+	*
 	*	Function Example:
 	* 	function sampleFunction(param1, param2){
 	*		// This is just a sample of a function setup
@@ -60,6 +60,27 @@
 	$('input[type=text]').on('keypress',checkCount);
 	$('input[type=text]').on('blur',checkCount);
 
-
+	/*******************************************************************
+	*	Artificial Social Defer
+	*		- Just showing function structure
+	*		- If the function has parameters, please list and explain
+	*		  what the parameters are for. NOTE: Params and Param
+	*		  explanations are examples only
+	*	Params:
+	*		@param1 - string - should be the
+	*		@param2 - boolean - heading soughtstreet
+	*
+	*	Function Example:
+	* 	function sampleFunction(param1, param2){
+	*		// This is just a sample of a function setup
+	*	}
+	*******************************************************************/
+	function socialDefer(){
+		var inject = "(function() {	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })(); (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id;	js.src = \"//connect.facebook.net/en_US/all.js#xfbml=1\"; fjs.parentNode.insertBefore(js, fjs);	}(document, 'script', 'facebook-jssdk')); !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='//platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');";
+		$('#socialDefers').prepend(inject);
+	}
+	if($('#socialDefers').length){
+		socialDefer();
+	}
 
 })(jQuery, this, document);
