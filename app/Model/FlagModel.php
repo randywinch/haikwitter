eff8e561deec6b974c495f23ac352251472415b8
jayseeg <jake_gibbons@the1stmovement.com>
2012-04-08 19:24:11 -0600

    missed adding file

---
+++ app/Model/FlagModel.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Haiku Model
+ *
+ */
+class FlagModel extends AppModel {
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $useTable = 'flags';
+	public $name = 'Flag';
+
+}
